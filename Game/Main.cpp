aa2ede3334f2f9c812b8bb1596a9eca24e56b84b
acchang24 <acchang24@gmail.com>
2023-12-12 15:44:35 -0800

    simple window

--- Game/Main.cpp
+++ Game/Main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <unordered_map>
 #include "glad/glad.h"
 #include "GLFW/glfw3.h"
 
@@ -6,9 +7,15 @@
 #define HEIGHT 720
 
 void FrameBufferSizeCallBack(GLFWwindow* window, int width, int height);
+void ProcessInput(GLFWwindow* window);
+
+std::unordered_map<int, bool> test;
 
 int main()
 {
+	test[GLFW_KEY_ESCAPE] = false;
+	test[GLFW_KEY_SPACE] = false;
+
 	// Initialize GLFW
 	glfwInit();
 	// Set major/minor contexts to 4
@@ -51,10 +58,17 @@ int main()
 	// glfwWindowShouldClose() checks at the start of each loop iteration to see if GLFW needs to be closed
 	while (!glfwWindowShouldClose(window))
 	{
+		// Specify color to clear the screen
+		glClearColor(0.2f, 0.2f, 0.2f, 1.0f);
+		// Clear the color buffer
+		glClear(GL_COLOR_BUFFER_BIT);
+
 		// Check to see if any events are triggered (inputs) and updates the window state
 		glfwPollEvents();
 
-		// Swap the color buffer
+		ProcessInput(window);
+
+		// Swap the buffers
 		glfwSwapBuffers(window);
 	}
 
@@ -67,4 +81,22 @@ int main()
 void FrameBufferSizeCallBack(GLFWwindow* window, int width, int height)
 {
 	glViewport(0, 0, width, height);
+}
+
+
+void ProcessInput(GLFWwindow* window)
+{
+	if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+	{
+		glfwSetWindowShouldClose(window, true);
+	}
+	if (glfwGetKey(window, GLFW_KEY_SPACE) == GLFW_PRESS && !test[GLFW_KEY_SPACE])
+	{
+		test[GLFW_KEY_SPACE] = true;
+		std::cout << "space" << std::endl;
+	}
+	if (glfwGetKey(window, GLFW_KEY_SPACE) == GLFW_RELEASE && test[GLFW_KEY_SPACE])
+	{
+		test[GLFW_KEY_SPACE] = false;
+	}
 }
