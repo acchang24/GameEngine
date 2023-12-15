8a88cb3629506945bdd1defea11b4bc20c8a5939
acchang24 <acchang24@gmail.com>
2023-12-14 16:18:51 -0800

    Texture class

---
+++ Engine/Texture.h
@@ -0,0 +1,57 @@
+#pragma once
+#include <string>
+
+// Enum class for the different types of textures
+enum class TextureType
+{
+	Diffuse, // Normal texture
+	Specular // Specular maps
+};
+
+// The Texture class helps load image files with the
+// stb_image loader and saves image details. All texture
+// objects are referenced with an integer and provides
+// functions to help set currently bound texture.
+class Texture
+{
+public:
+	// Texture constructor takes the name of a texture file
+	// and creates an OpenGl texture object
+	// @param - const char* for the texture file name
+	Texture(const char* textureFile);
+	~Texture();
+
+	// Binds the texture as the current one
+	void SetActive();
+
+	// Getterf for the texture's ID
+	// @returns - unsinged int mTextureID;
+	unsigned int GetID() const { return mTextureID; }
+
+	// Getter for the texture type
+	// @returns - TextureType mType
+	TextureType GetType() const { return mType; }
+
+	// Setter for the texture type
+	// @param - TextureType for the texture type
+	void SetType(TextureType type) { mType = type; }
+
+private:
+	// Texture name (file path to the texture)
+	std::string mName;
+
+	// Texture id used for reference
+	unsigned int mTextureID;
+
+	// Width of the texture
+	int mWidth;
+
+	// Height of the texture
+	int mHeight;
+
+	// Number of color channels
+	int mNumChannels;
+
+	// Type of the texture
+	TextureType mType;
+};
