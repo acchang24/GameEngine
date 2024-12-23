be942a78a0f872044f014d2e31b9f21885387d18
acchang24 <acchang24@gmail.com>
2024-12-23 03:11:28 -0800

    set PointShadows active

--- Engine/Source/Graphics/PointShadowMap.cpp
+++ Engine/Source/Graphics/PointShadowMap.cpp
@@ -1,12 +1,19 @@
 #include "PointShadowMap.h"
 #include <iostream>
 #include <glad/glad.h>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
 #include "Texture.h"
+#include "UniformBuffer.h"
 
 const unsigned int SHADOW_WIDTH = 8560;
 const unsigned int SHADOW_HEIGHT = 8560;
+float nearPlane = 1.0f;
+float farPlane = 100.0f;
 
 PointShadowMap::PointShadowMap() :
+	mPointShadowConsts({}),
+	mPointShadowBuffer(new UniformBuffer(sizeof(PointShadowMapConsts), BufferBindingPoint::PointShadow, "PointShadowBuffer")),
 	mPointShadowMapFrameBuffer(0),
 	mPointShadowMap(0),
 	mTextureUnit(static_cast<int>(TextureUnit::PointShadow))
@@ -43,4 +50,40 @@ PointShadowMap::~PointShadowMap()
 	glDeleteFramebuffers(1, &mPointShadowMapFrameBuffer);
 
 	glDeleteTextures(1, &mPointShadowMap);
+}
+
+void PointShadowMap::SetActive(float near, float far, const glm::vec3 pos)
+{
+	float aspect = static_cast<float>(SHADOW_WIDTH / SHADOW_HEIGHT);
+
+	float nearPlane = near;
+	float farPlane = far;
+
+	glm::mat4 shadowProj = glm::perspective(glm::radians(90.0f), aspect, near, far);
+
+	mPointShadowConsts.shadowTransforms[0] = shadowProj * glm::lookAt(pos, pos + glm::vec3(1.0f, 0.0f, 0.0f), glm::vec3(0.0f, -1.0f, 0.0f));
+	mPointShadowConsts.shadowTransforms[1] = shadowProj * glm::lookAt(pos, pos + glm::vec3(-1.0f, 0.0f, 0.0f), glm::vec3(0.0f, -1.0f, 0.0f));
+	mPointShadowConsts.shadowTransforms[2] = shadowProj * glm::lookAt(pos, pos + glm::vec3(0.0f, 1.0f, 0.0f), glm::vec3(0.0f, 0.0f, 1.0f));
+	mPointShadowConsts.shadowTransforms[3] = shadowProj * glm::lookAt(pos, pos + glm::vec3(0.0f, -1.0f, 0.0f), glm::vec3(0.0f, 0.0f, -1.0f));
+	mPointShadowConsts.shadowTransforms[4] = shadowProj * glm::lookAt(pos, pos + glm::vec3(0.0f, 0.0f, 1.0f), glm::vec3(0.0f, -1.0f, 0.0f));
+	mPointShadowConsts.shadowTransforms[5] = shadowProj * glm::lookAt(pos, pos + glm::vec3(0.0f, 0.0f, -1.0f), glm::vec3(0.0f, -1.0f, 0.0f));
+
+	// First pass will render to shadow map
+	glViewport(0, 0, SHADOW_WIDTH, SHADOW_HEIGHT);
+
+	// Bind to frame buffer
+	glBindFramebuffer(GL_FRAMEBUFFER, mPointShadowMapFrameBuffer);
+	// Clear depth buffer
+	glClear(GL_DEPTH_BUFFER_BIT);
+
+	mPointShadowBuffer->UpdateBufferData(&mPointShadowConsts);
+}
+
+void PointShadowMap::End(int width, int height) const
+{
+	// Bind back to default frame buffer
+	glBindFramebuffer(GL_FRAMEBUFFER, 0);
+	// Set viewport back to screen's width and height
+	glViewport(0, 0, width, height);
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 }
--- Engine/Source/Graphics/PointShadowMap.h
+++ Engine/Source/Graphics/PointShadowMap.h
@@ -6,13 +6,23 @@ struct PointShadowMapConsts
 	glm::mat4 shadowTransforms[6];
 };
 
+class UniformBuffer;
+
 class PointShadowMap
 {
 public:
 	PointShadowMap();
 	~PointShadowMap();
 
+	void SetActive(float near, float far, const glm::vec3 pos);
+
+	void End(int width, int height) const;
+
 private:
+	PointShadowMapConsts mPointShadowConsts;
+
+	UniformBuffer* mPointShadowBuffer;
+
 	// Frame buffer for point shadow map
 	unsigned int mPointShadowMapFrameBuffer;
 
