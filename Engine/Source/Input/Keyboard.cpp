e3caa2f39dfff20b1536c8d0b0ca9f1d28a9904c
acchang24 <acchang24@gmail.com>
2025-06-18 02:19:49 -0700

    reset keyboard state

--- Engine/Source/Input/Keyboard.cpp
+++ Engine/Source/Input/Keyboard.cpp
@@ -16,6 +16,11 @@ const Uint8* Keyboard::GetState()
 	return SDL_GetKeyboardState(NULL);
 }
 
+bool Keyboard::PressedLastFrame(SDL_Scancode code) const
+{
+	return mPrevKeyInputs[code];
+}
+
 bool Keyboard::HasLeadingEdge(const Uint8* state, SDL_Scancode code) const
 {
 	return (state[code] && !mPrevKeyInputs[code]);
@@ -24,4 +29,9 @@ bool Keyboard::HasLeadingEdge(const Uint8* state, SDL_Scancode code) const
 void Keyboard::SavePrevKeyState(const Uint8* state, SDL_Scancode code)
 {
 	mPrevKeyInputs[code] = state[code];
+}
+
+void Keyboard::ResetKeyboard()
+{
+	SDL_ResetKeyboard();
 }
--- Engine/Source/Input/Keyboard.h
+++ Engine/Source/Input/Keyboard.h
@@ -10,13 +10,18 @@ public:
 	// Gets the keyboard's state
 	const Uint8* GetState();
 
+	// Detects if a key was pressed in the last frame
+	bool PressedLastFrame(SDL_Scancode code) const;
+
 	// Detects if a key is currently pressed, while being unpressed in the last frame
 	bool HasLeadingEdge(const Uint8* state, SDL_Scancode code) const;
 
 	// Saves a key's state for reference in the next frame (use at end of ProcessInput)
 	void SavePrevKeyState(const Uint8* state, SDL_Scancode code);
 
+	void ResetKeyboard();
+
 private:
 	// Array of previous key inputs to see if they are pressed or not
-	bool mPrevKeyInputs[SDL_NUM_SCANCODES];
+	bool mPrevKeyInputs[SDL_NUM_SCANCODES / 2];
 };
